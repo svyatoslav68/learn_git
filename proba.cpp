f0d7e105acd964201be9b341a16ee68ba4e3d1a0
Svyatoslav <svyatoslav68@icloud.com>
2021-01-06 22:32:30 +0300

    Созданы первые файлы проекта.

---
+++ proba.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int foo(const std::string &input_str)
+{
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hellow, world" << std::endl;
+}
+
